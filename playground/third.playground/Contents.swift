d34c3a46748633aecf6c7726f500c3d968444134
Margaro <sichibukay@icloud.com>
2018-02-17 14:47:29 -0600

    the quiz project was added

---
+++ playground/third.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func factorial(n: Int) -> Int{
+  if n == 0 || n == 1 {
+    return 1
+  }else{
+    return n * factorial(n: n-1)
+  }
+}
+
+factorial(n: 6)
